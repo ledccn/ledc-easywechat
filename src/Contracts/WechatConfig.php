952c1da98d7f6c0cef055e1a04763554d0437a34
david <367013672@qq.com>
2025-07-29 23:26:39 +0800

    适配：php 8.4

--- src/Contracts/WechatConfig.php
+++ src/Contracts/WechatConfig.php
@@ -15,5 +15,5 @@ interface WechatConfig
      * @param int|string|Request|\support\Request|null $key 终端支付渠道
      * @return array|ConfigInterface
      */
-    public function get(int|string|Request|\support\Request $key = null): array|ConfigInterface;
+    public function get(int|string|Request|\support\Request|null $key = null): array|ConfigInterface;
 }
